1493967e0b036655f52e4fbde511019239a068b6
Greg Scott <me@gregoryjscott.com>
2017-06-30 19:11:49 -0500

    Randomly pick pronoun for abrir

--- spanish.js
+++ spanish.js
@@ -1,5 +1,14 @@
 const pronouns = [
-  'Yo'
+  'Yo',
+  'Tu',
+  'El',
+  'Ella',
+  'Usted',
+  'Nosotros',
+  'Nosotras',
+  'Ellos',
+  'Ellas',
+  'Ustedes'
 ]
 
 const verbs = [
@@ -8,21 +17,65 @@ const verbs = [
 
 let pronoun, verb, answer
 
+function pick(max) {
+  return Math.floor(Math.random() * max)
+}
+
 function askQuestion() {
-  pronoun = pronouns[0]
+  pronoun = pronouns[pick(pronouns.length - 1)]
   verb = verbs[0]
 
   console.log(`Conjugate the verb "${verb}" for the pronoun "${pronoun}".`)
 }
 
 function checkAnswer(answer) {
-  let correctAnswer = 'abro'
+  let correctAnswer = determineCorrectAnswer(verb)
 
   if (answer === correctAnswer) {
-    console.log(`"${pronoun} ${answer}" is correct!\n`)
+    console.log(`Si, Si, Si! "${pronoun} ${answer}" is correct!\n`)
   } else {
-    console.log(`"${answer}" is wrong. The correct conjugation is "${pronoun} ${correctAnswer}".\n`)
+    console.log(`No. It's ${pronoun} "${correctAnswer}".\n`)
   }
+}
+
+function determineCorrectAnswer() {
+  let start, middle
+
+  start = verb.substr(0, verb.length - 2)
+
+  if (verb.endsWith('ar')) {
+    middle = 'a'
+  } else if (verb.endsWith('er')) {
+    middle = 'e'
+  } else if (verb.endsWith('ir')) {
+    middle = pronoun.startsWith('Nosotr') ? 'i' : 'e'
+  } else {
+    bomb()
+  }
+
+  switch (pronoun) {
+    case 'Yo':
+      return `${start}o`
+    case 'Tu':
+      return `${start}${middle}s`
+    case 'El':
+    case 'Ella':
+    case 'Usted':
+      return `${start}${middle}`
+    case 'Nosotros':
+    case 'Nosotras':
+      return `${start}${middle}mos`
+    case 'Ellos':
+    case 'Ellas':
+    case 'Ustedes':
+      return `${start}${middle}n`
+    default:
+      bomb()
+  }
+}
+
+function bomb() {
+  throw new Error(`This must be Spanish I haven't learned about yet. Sorry.`)
 }
 
 process.stdin.setEncoding('utf8')

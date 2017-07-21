ae7795b468dc1ee04781408a0621fc21746d62b7
Greg Scott <me@gregoryjscott.com>
2017-07-21 15:34:46 -0500

    Ask for answers to information questions

--- informational.js
+++ informational.js
@@ -1,18 +1,81 @@
+const colors = require('colors/safe')
 const define = require('./define')
+const random = require('./random')
 
 const informational = [
-  { spanish: 'Como se llama?', english: 'What is your name?' },
-  { spanish: 'Como esta?', english: 'How are you?' },
-  { spanish: 'Donde vive?', english: 'Where do you live?' },
-  { spanish: 'De donde es?', english: 'Where are you from?' },
-  { spanish: 'Cual es su numero de telefono?', english: 'What is your telephone number?' },
-  { spanish: 'Cual es su dirrecion?', english: 'What is your address?' }
+  {
+    spanish: 'Como se llama?',
+    english: 'What is your name?',
+    checkAnswer: answer => answerStartsWith(answer, 'Yo me llamo')
+  },
+  {
+    spanish: 'Como esta?',
+    english: 'How are you?',
+    checkAnswer: answer => {
+      const correctAnswers = ['bien', 'mal']
+      if (correctAnswers.includes(answer.toLowerCase())) {
+        console.log(colors.green(`Si, si, si! "${answer}" is correct!\n`))
+        return true
+      } else {
+        console.log(colors.red(`No. The answer should be one of the following: ${correctAnswers.join(', ')}.\n`))
+        return false
+      }
+    }
+  },
+  {
+    spanish: 'Donde vive?',
+    english: 'Where do you live?',
+    checkAnswer: answer => answerStartsWith(answer, 'Yo vivo en')
+  },
+  {
+    spanish: 'De donde es?',
+    english: 'Where are you from?',
+    checkAnswer: answer => answerStartsWith(answer, 'Yo soy de')
+  },
+  {
+    spanish: 'Cual es su numero de telefono?',
+    english: 'What is your telephone number?',
+    checkAnswer: answer => answerStartsWith(answer, 'Mi numero es')
+  },
+  {
+    spanish: 'Cual es su direcion?',
+    english: 'What is your address?',
+    checkAnswer: answer => answerStartsWith(answer, 'Me direccion es')
+  }
 ]
 
+function answerStartsWith(answer, beginning) {
+  if (answer.toLowerCase().startsWith(beginning.toLowerCase())) {
+    console.log(colors.green(`Si, si, si! "${answer}" is correct!\n`))
+    return true
+  } else {
+    console.log(colors.red(`No. The answer should start with "${beginning}".\n`))
+    return false
+  }
+}
+
+const quizzes = [ 'definitions', 'answerQuestionsInSpanish']
 const definitions = define(informational)
+let currentQuiz, questionToAnswer
 
 module.exports = {
   name: 'Informational',
-  askQuestion: definitions.askQuestion,
-  checkAnswer: definitions.checkAnswer
+  askQuestion: () => {
+    currentQuiz = random(quizzes)
+
+    if (currentQuiz === 'definitions') {
+      definitions.askQuestion()
+    } else {
+      questionToAnswer = random(informational)
+      console.log(`Answer this question in Spanish: "${questionToAnswer.spanish}".`)
+    }
+  },
+
+  checkAnswer: answer => {
+    if (currentQuiz === 'definitions') {
+      return definitions.checkAnswer(answer)
+    } else {
+      return questionToAnswer.checkAnswer(answer)
+    }
+  }
 }

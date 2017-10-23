6bb3b8f6f76eb43ac734b8c986cd1166cd8062eb
Greg Scott <me@gregoryjscott.com>
2017-10-23 15:08:49 -0500

    Change irregular conjugations to handle all pronouns

--- quizzes/irregular-conjugations.js
+++ quizzes/irregular-conjugations.js
@@ -3,14 +3,13 @@ const pronouns = require('./pronouns')
 const verbs = require('./irregular-verbs')
 const random = require('../random')
 
-let verb
+let pronoun, verb
 
 function askQuestion() {
+  pronoun = random(pronouns.words)
   verb = random(verbs.words)
 
-  console.log(
-    `Conjugate the verb "${verb.spanish}" for the pronoun Yo.`
-  )
+  console.log(`Conjugate the verb "${verb.spanish}" for the pronoun "${pronoun.spanish}".`)
 }
 
 function checkAnswer(answer) {
@@ -18,19 +17,57 @@ function checkAnswer(answer) {
 
   if (answer === correctAnswer) {
     console.log(
-      colors.green(`Si, Si, Si! "Yo ${answer}" is correct!\n`)
+      colors.green(`Si, Si, Si! "${pronoun.spanish} ${answer}" is correct!\n`)
     )
     return true
   } else {
-    console.log(colors.red(`No. It's Yo "${correctAnswer}".\n`))
+    console.log(colors.red(`No. It's ${pronoun.spanish} "${correctAnswer}".\n`))
     return false
   }
 }
 
 function determineCorrectAnswer() {
   const start = verb.spanish.substr(0, verb.spanish.length - 2)
+  let middle
 
-  return verb.yo || `${start}o`
+  if (verb.spanish.endsWith('ar')) {
+    middle = 'a'
+  } else if (verb.spanish.endsWith('er')) {
+    middle = 'e'
+  } else if (verb.spanish.endsWith('ir')) {
+    middle = pronoun.spanish.startsWith('Nosotr') ? 'i' : 'e'
+  } else {
+    oops()
+  }
+
+  switch (pronoun.spanish) {
+    case 'Yo':
+      return verb.yo || `${start}o`
+    case 'Tu':
+      return verb.tu || `${start}${middle}s`
+    case 'El':
+      return verb.el || `${start}${middle}`
+    case 'Ella':
+      return verb.ella || `${start}${middle}`
+    case 'Usted':
+      return verb.usted || `${start}${middle}`
+    case 'Nosotros':
+      return verb.nosotros || `${start}${middle}mos`
+    case 'Nosotras':
+      return verb.nosotras || `${start}${middle}mos`
+    case 'Ellos':
+      return verb.ellos || `${start}${middle}n`
+    case 'Ellas':
+      return verb.ellas || `${start}${middle}n`
+    case 'Ustedes':
+      return verb.ustedes || `${start}${middle}n`
+    default:
+      oops()
+  }
+}
+
+function oops() {
+  throw new Error(`Something went wrong.`)
 }
 
 module.exports = {

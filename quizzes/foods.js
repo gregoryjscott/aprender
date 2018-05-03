0c9066740a9dcf9c94fec8376ed86db6545ba2f1
Greg Scott <me@gregoryjscott.com>
2018-05-03 13:18:22 -0500

    Add gender to foods

--- quizzes/foods.js
+++ quizzes/foods.js
@@ -1,27 +1,27 @@
 const define = require('../define')
 
 const food = [
-  { spanish: 'cafe', english: 'coffee' },
-  { spanish: 'mantequilla', english: 'butter' },
-  { spanish: 'pan', english: 'bread' },
-  { spanish: 'huevos', english: 'eggs' },
-  { spanish: 'dulces', english: 'candy' },
-  { spanish: 'frutas', english: 'fruits' },
-  { spanish: 'manzanas', english: 'apples' },
-  { spanish: 'naranjas', english: 'oranges' },
-  { spanish: 'jugo de naranja', english: 'orange juice' },
-  { spanish: 'queso', english: 'cheese' },
-  { spanish: 'leche', english: 'milk' },
-  { spanish: 'helado de chocolate', english: 'chocolate ice cream' },
-  { spanish: 'helado de vanilla', english: 'vanilla ice cream' },
-  { spanish: 'pollo', english: 'chicken' },
-  { spanish: 'carne', english: 'meat' },
-  { spanish: 'pescada', english: 'fish' },
-  { spanish: 'verduras', english: 'vegetables' },
-  { spanish: 'sopa', english: 'soup' },
-  { spanish: 'ensalada', english: 'salad' },
-  { spanish: 'arroz', english: 'rice' },
-  { spanish: 'postre', english: 'dessert' }
+  { spanish: 'el cafe', english: 'coffee' },
+  { spanish: 'la mantequilla', english: 'butter' },
+  { spanish: 'el pan', english: 'bread' },
+  { spanish: 'el huevo', english: 'egg' },
+  { spanish: 'los dulces', english: 'candy' },
+  { spanish: 'las frutas', english: 'fruits' },
+  { spanish: 'la manzana', english: 'apple' },
+  { spanish: 'la naranja', english: 'orange' },
+  { spanish: 'el jugo de naranja', english: 'orange juice' },
+  { spanish: 'el queso', english: 'cheese' },
+  { spanish: 'la leche', english: 'milk' },
+  { spanish: 'el helado de chocolate', english: 'chocolate ice cream' },
+  { spanish: 'el helado de vanilla', english: 'vanilla ice cream' },
+  { spanish: 'el pollo', english: 'chicken' },
+  { spanish: 'la carne', english: 'meat' },
+  { spanish: 'el pescada', english: 'fish' },
+  { spanish: 'las verduras', english: 'vegetables' },
+  { spanish: 'la sopa', english: 'soup' },
+  { spanish: 'la ensalada', english: 'salad' },
+  { spanish: 'el arroz', english: 'rice' },
+  { spanish: 'el postre', english: 'dessert' }
 ]
 const definitions = define(food)
 
